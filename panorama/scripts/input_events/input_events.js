e1aae6464cbd1da82b8e06dbe49bff92fa13cbf6
Liam Murray <rodionskies@gmail.com>
2022-10-30 13:13:03 -0400

    initial commit

---
+++ panorama/scripts/input_events/input_events.js
@@ -0,0 +1,129 @@
+const OutputDebugInfo = true
+
+const Print = (String) => {
+    if (OutputDebugInfo)
+        $.Msg(String)
+}
+
+const GetHotReloadPersistentStorage = () => $.GetContextPanel()
+
+const GetTimeStamp = () => {
+    const Storage = GetHotReloadPersistentStorage()
+    return Storage.TimeStamp ? Storage.TimeStamp : Storage.TimeStamp = Date.now()
+}
+
+const GetRegisteredKeyBinds = () => {
+    const Storage = GetHotReloadPersistentStorage()
+    return Storage.RegisteredKeyBinds ? Storage.RegisteredKeyBinds : Storage.RegisteredKeyBinds = {}
+}
+
+function MulticastDelegate () {
+    this.EventHandlers = {}
+    this.PreviousId = 0
+    this.GetNewId = () => ++this.PreviousId
+    this.Remove = (EventId) => delete this.EventHandlers[EventId]
+    this.Add = (Handler) => {
+        const EventId = this.GetNewId()
+        this.EventHandlers[EventId] = Handler
+        return EventId
+    }
+    this.Empty = () => {
+        this.EventHandlers = {}
+        this.PreviousId = 0
+    }
+    this.Execute = () => {
+        for (const [_, Handler] of Object.entries(this.EventHandlers))
+            Handler()
+    }
+}
+
+const GetCommandDelegates = () => {
+    const Storage = GetHotReloadPersistentStorage()
+    return Storage.CommandDelegates ? Storage.CommandDelegates : Storage.CommandDelegates = {}
+}
+
+const GetCommandDelegate = (CommandId) => {
+    const CommandDelegates = GetCommandDelegates()
+    return CommandDelegates[CommandId] ?  CommandDelegates[CommandId] :  CommandDelegates[CommandId] = new MulticastDelegate()
+}
+
+const GetKeyBindForCommand = (DOTAKeybindCommand) => Game.GetKeybindForCommand(DOTAKeybindCommand).replace("ALT-", "ALT+"); // CreateCustomKeyBind will work as expected with KeyBinds that use the ALT modifier
+
+const InvokeCommand = (CommandId) => GetCommandDelegate(CommandId).Execute()
+
+const ClearAllExistingKeyBindCallbacks = () => {
+    const RegisteredKeyBinds = GetRegisteredKeyBinds()
+    for (const KeyBind in RegisteredKeyBinds) {
+        RegisteredKeyBinds[KeyBind].Callback = () => {}
+    }
+}
+
+// In order to support user changing their KeyBinds, KeyBinds can only be setup to work for one of these layers
+// This is due to the workaround used for the CreateCustomKeyBind bug where old CreateCustomKeyBinds cannot be removed 
+// This project is currently hard coded to use MainLayer since it has the most KeyBinds and suits most use cases
+// "Layer" : KeyBinds on different layers can have duplicate keys bound
+const EKeyBindLayers = {
+    MainLayer : "MainLayer",
+    ShopLayer : "ShopLayer",
+    SpectatorLayer : "SpectatorLayer"
+}
+
+const IsValidCommand = (CommandId) => CommandId > DOTAKeybindCommand_t.DOTA_KEYBIND_FIRST && CommandId < DOTAKeybindCommand_t.DOTA_KEYBIND_COUNT
+const IsShopLayerCommand = (ComandId) => ComandId >= DOTAKeybindCommand_t.DOTA_KEYBIND_SHOP_CONSUMABLES && ComandId <= DOTAKeybindCommand_t.DOTA_KEYBIND_SHOP_SLOT_14
+const IsSpectatorLayerCommand = (CommandId) => CommandId >= DOTAKeybindCommand_t.DOTA_KEYBIND_SPEC_CAMERA_UP && CommandId <= DOTAKeybindCommand_t.DOTA_KEYBIND_SPEC_COACH_VIEWTOGGLE 
+const IsMainLayerCommand = (CommandId) => IsValidCommand(CommandId) && !IsShopLayerCommand(CommandId) && !IsSpectatorLayerCommand(CommandId)
+
+const RegisterKeybinds = () => {
+    ClearAllExistingKeyBindCallbacks()
+    
+    for (const Property in DOTAKeybindCommand_t)
+    {
+        const CommandId = DOTAKeybindCommand_t[Property]
+        
+        if (!IsMainLayerCommand(CommandId))
+            continue
+        
+        if (!GameUI.CustomUIConfig().CommandEvents.FilterKeyBind(CommandId))
+            continue
+        
+        const KeyBind = GetKeyBindForCommand(CommandId)
+        if (!KeyBind || KeyBind === "")
+            continue 
+        
+        const RegisteredKeyBinds = GetRegisteredKeyBinds()
+        const RegisteredKeyBind = RegisteredKeyBinds[KeyBind]
+        if (!RegisteredKeyBind) {
+            RegisteredKeyBinds[KeyBind] = {}
+            RegisteredKeyBinds[KeyBind].Callback = () => InvokeCommand(CommandId)
+
+            const CommandName = `CommandHook_${EKeyBindLayers.MainLayer}_KeyBind_${KeyBind}_${GetTimeStamp()}`
+            Print("command name: " + CommandName)
+            
+            Game.CreateCustomKeyBind(KeyBind, CommandName);
+            Game.AddCommand(CommandName, () => {
+                Print(`Attempting to call command for keybind ${KeyBind}`)
+                RegisteredKeyBinds[KeyBind].Callback()
+            }, "", 0);
+            Print(`Registering KeyBind ${KeyBind} to call command ${CommandId}`)
+        } else {
+            RegisteredKeyBinds[KeyBind].Callback = () => InvokeCommand(CommandId)
+            Print(`Re-registering KeyBind ${KeyBind} to call command ${CommandId}`)
+        }
+    }
+}
+
+(() => {
+    if (!GameUI.CustomUIConfig().CommandEvents) {
+        function CommandDelegateHandle (CommandId, DelegateHandle) {
+            this.CommandId = CommandId
+            this.DelegateHandle = DelegateHandle
+        }
+        const CommandEvents = GameUI.CustomUIConfig().CommandEvents = {};
+        CommandEvents.FilterKeyBind = (CommandId) => false // by default we don't register any keybinds
+        CommandEvents.RegisterKeyBinds = RegisterKeybinds
+        CommandEvents.Add = (CommandId, Handler) => new CommandDelegateHandle(CommandId, GetCommandDelegate(CommandId).Add(Handler))
+        CommandEvents.Remove = (CommandDelegateHandle) => GetCommandDelegate(CommandDelegateHandle.CommandId).Remove(CommandDelegateHandle.DelegateHandle)
+        CommandEvents.Empty = (CommandId) => GetCommandDelegate(CommandId).Empty()
+        $.RegisterForUnhandledEvent('DOTASettingsChanged', RegisterKeybinds) // Registered KeyBinds will update automatically when the user changes them in the settings
+    }
+})()
---
+++ panorama/scripts/input_events/input_events_test.js
@@ -0,0 +1,37 @@
+﻿(() => {
+    const CommandEvents = GameUI.CustomUIConfig().CommandEvents
+
+    // Filter by spell keybinds
+    // Registering a KeyBind will cause it to overwrite the default behavior, so we add a filter and only choose the ones we want.
+    // Note that if you change the filter and re-register it will not bring back the default behavior of the hero since there is no way to undo Game.CreateCustomKeyBind 
+    CommandEvents.FilterKeyBind = (CommandId) => CommandId >= DOTAKeybindCommand_t.DOTA_KEYBIND_ABILITY_PRIMARY1 && CommandId <= DOTAKeybindCommand_t.DOTA_KEYBIND_ABILITY_ULTIMATE_QUICKCAST
+    CommandEvents.RegisterKeyBinds() 
+    
+    if (!CommandEvents.Test1) {
+        Print("Running Input Events Test")
+        
+        CommandEvents.Test1 = {}
+        
+        // create callbacks for quick cast spell 1, 2, and 3
+        const Ability1 = DOTAKeybindCommand_t.DOTA_KEYBIND_ABILITY_PRIMARY1_QUICKCAST
+        const Ability2 = DOTAKeybindCommand_t.DOTA_KEYBIND_ABILITY_PRIMARY2_QUICKCAST
+        const Ability3 = DOTAKeybindCommand_t.DOTA_KEYBIND_ABILITY_PRIMARY3_QUICKCAST
+
+        CommandEvents.Add(Ability1, () => Print("Ability 1 Has Been Cast"))
+
+        const DelegateHandle = CommandEvents.Add(Ability1, () => Print("Another Ability 1 Handler"))
+
+        CommandEvents.Add(Ability2, () => Print("Ability 2 Has Been Cast"))
+
+        CommandEvents.Add(Ability2, () => {
+            Print("Ability 2 Handler")
+            CommandEvents.Remove(DelegateHandle) // remove just this event callback for ability 1
+        })
+
+        CommandEvents.Add(Ability3, () => {
+            Print("Ability 3 Handler")
+            CommandEvents.Empty(Ability1) // remove all event callbacks for ability 1
+            CommandEvents.Empty(Ability2)// remove all event callbacks for ability 2
+        })
+    }
+})()
